e5b8a24f90ef3cd1f6319d5aaf08e55c862fd29e
ElbUko <jadegu85@gmail.com>
2017-02-28 21:44:51 +0100

    traigo lo viejo en php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+
+#Localhost
+$localhost = 'http://localhost/juegoskeleto/';
+
+#Datos de conexion
+$host = 'localhost'; 
+$root = 'root'; 
+$clave = 'bukobuko'; 
+$db = 'prueba1';
+define('HOST', 'localhost');
+define('ROOT', 'root');
+define('CLAVE', 'bukobuko');
+define('DB', 'prueba1');
+?>
